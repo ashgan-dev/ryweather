093faa50872951224d2116165767649264ae54de
ashgan-dev <ashgan-dev@users.noreply.github.com>
2021-05-06 22:33:48 +0200

    ajout d'un theme fait pas Soleya
    ajout d'un theme,
    correction des heures de référence

--- ryweather.py
+++ ryweather.py
@@ -2,40 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import json
+from datetime import datetime
 from decimal import Decimal
-from flask import Flask, render_template
 
+import arrow
 import requests
+from dateutil.rrule import rrule, MINUTELY
+from flask import Flask, render_template
 
 
 def get_cc():
-    response = requests.get('https://api.bmsite.net/atys/weather?cycles=40&offset=0')
+    response = requests.get('https://api.bmsite.net/atys/weather?cycles=40&offset=1')
     decoded = json.loads(response.text)
 
-    start_hour = Decimal(decoded['hour'])
-    ig_hours = int(start_hour) % 24
-    ig_min = int((start_hour - int(start_hour)) * 60)
-    ig_time = '{}:{}'.format(ig_hours, ig_min)
-
     data = dict()
+    data['cycles'] = list()
+    data['cycles'].append(arrow.utcnow().to('Europe/Paris').shift(minutes=-3).format('HH:mm'))
+    for dt in rrule(MINUTELY, dtstart=datetime.now(), interval=3, count=79):
+        data['cycles'].append(arrow.get(dt).to('Europe/Paris').format('HH:mm'))
+
     for j in decoded['continents'].keys():
         data[j] = list()
-        data['ig-start_time'] = ig_time
-        data['cycles'] = list()
-        cycle_en_cours = ig_hours
+
         for k in decoded['continents'][j].values():
-            data['cycles'].append(cycle_en_cours)
             data[j].append(float(Decimal(k['value']) * 100))
-            if cycle_en_cours == 23:
-                cycle_en_cours = 0
-            else:
-                cycle_en_cours += 1
             data[j].append(float(Decimal(k['value']) * 100))
-            data['cycles'].append(cycle_en_cours)
-            if cycle_en_cours == 23:
-                cycle_en_cours = 0
-            else:
-                cycle_en_cours += 1
+
     return data
 
 
@@ -45,7 +37,8 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def start_page():
     data = get_cc()
-    print(data)
+    with open('/tmp/data.js', 'w') as data_file:
+        json.dump(data, data_file)
     return render_template('index.html',
                            data=data)
 
