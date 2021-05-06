59d3d2cd89d96e5989eb8731c8204a1d032b4bc2
ashgan-dev <ashgan-dev@users.noreply.github.com>
2021-05-06 16:14:51 +0000

    Update ryweather.py

--- ryweather.py
+++ ryweather.py
@@ -12,16 +12,30 @@ def get_cc():
     response = requests.get('https://api.bmsite.net/atys/weather?cycles=40&offset=0')
     decoded = json.loads(response.text)
 
+    start_hour = Decimal(decoded['hour'])
+    ig_hours = int(start_hour) % 24
+    ig_min = int((start_hour - int(start_hour)) * 60)
+    ig_time = '{}:{}'.format(ig_hours, ig_min)
+
     data = dict()
     for j in decoded['continents'].keys():
         data[j] = list()
-        data['cycles'] = set()
+        data['ig-start_time'] = ig_time
+        data['cycles'] = list()
+        cycle_en_cours = ig_hours
         for k in decoded['continents'][j].values():
-            data['cycles'].add(k['cycle'])
+            data['cycles'].append(cycle_en_cours)
             data[j].append(float(Decimal(k['value']) * 100))
+            if cycle_en_cours == 23:
+                cycle_en_cours = 0
+            else:
+                cycle_en_cours += 1
             data[j].append(float(Decimal(k['value']) * 100))
-
-    data['cycles'] = list(data['cycles'])
+            data['cycles'].append(cycle_en_cours)
+            if cycle_en_cours == 23:
+                cycle_en_cours = 0
+            else:
+                cycle_en_cours += 1
     return data
 
 
