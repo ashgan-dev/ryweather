985c78f65af1daabefd328c6db2ef335cea1bb49
ashgan-dev <ashgan-dev@users.noreply.github.com>
2021-05-07 14:38:33 +0200

    redecoupage des cycles
    redecoupage des cycles pour mieux respecter les enchainements CC/transition.
    revue des heures de transition pour mieux coller aux dates in game.

--- ryweather.py
+++ ryweather.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 from decimal import Decimal
 
 import arrow
@@ -12,21 +12,60 @@ from flask import Flask, render_template
 
 
 def get_cc():
-    response = requests.get('https://api.bmsite.net/atys/weather?cycles=40&offset=1')
+    response = requests.get('https://api.bmsite.net/atys/weather?cycles=40&offset=0')
     decoded = json.loads(response.text)
 
     data = dict()
     data['cycles'] = list()
-    data['cycles'].append(arrow.utcnow().to('Europe/Paris').shift(minutes=-3).format('HH:mm'))
-    for dt in rrule(MINUTELY, dtstart=datetime.now(), interval=3, count=79):
-        data['cycles'].append(arrow.get(dt).to('Europe/Paris').format('HH:mm'))
 
-    for j in decoded['continents'].keys():
-        data[j] = list()
+    for dt in rrule(MINUTELY, dtstart=datetime.utcnow(), interval=3, count=120):
+        data['cycles'].append(arrow.get(dt).to('Europe/Paris').format('HH:mm:ss'))
 
-        for k in decoded['continents'][j].values():
-            data[j].append(float(Decimal(k['value']) * 100))
-            data[j].append(float(Decimal(k['value']) * 100))
+    cycle_courant = str(decoded['cycle'])
+    ingame_hour = Decimal(decoded['hour'])
+    ingame_min = ingame_hour - int(ingame_hour)
+
+    min_left = 60 - int(ingame_min * 60)
+    real_delta_time = timedelta(seconds=min_left * 0.05 * 60)
+    next_cc = arrow.utcnow() + real_delta_time
+
+    if int(ingame_hour) % 3 == 0:
+        for i in decoded['continents'].keys():
+            data[i] = list()
+            popped_cycle = decoded['continents'][i].pop(cycle_courant)
+
+            data[i].append(float(Decimal(popped_cycle['value']) * 100))
+            data[i].append(float(Decimal(popped_cycle['value']) * 100))
+            data[i].append(float(Decimal(popped_cycle['value']) * 100))
+
+            for j in decoded['continents'][i].values():
+                data[i].append(float(Decimal(j['value']) * 100))
+                data[i].append(float(Decimal(j['value']) * 100))
+                data[i].append(float(Decimal(j['value']) * 100))
+    elif int(ingame_hour) % 3 == 1:
+        for i in decoded['continents'].keys():
+            data[i] = list()
+            popped_cycle = decoded['continents'][i].pop(cycle_courant)
+
+            data[i].append(float(Decimal(popped_cycle['value']) * 100))
+            data[i].append(float(Decimal(popped_cycle['value']) * 100))
+
+            for j in decoded['continents'][i].values():
+                data[i].append(float(Decimal(j['value']) * 100))
+                data[i].append(float(Decimal(j['value']) * 100))
+                data[i].append(float(Decimal(j['value']) * 100))
+    else:
+        print('etat: transition')
+        for i in decoded['continents'].keys():
+            data[i] = list()
+            popped_cycle = decoded['continents'][i].pop(cycle_courant)
+
+            data[i].append(float(Decimal(popped_cycle['value']) * 100))
+
+            for j in decoded['continents'][i].values():
+                data[i].append(float(Decimal(j['value']) * 100))
+                data[i].append(float(Decimal(j['value']) * 100))
+                data[i].append(float(Decimal(j['value']) * 100))
 
     return data
 
@@ -37,8 +76,6 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def start_page():
     data = get_cc()
-    with open('/tmp/data.js', 'w') as data_file:
-        json.dump(data, data_file)
     return render_template('index.html',
                            data=data)
 
