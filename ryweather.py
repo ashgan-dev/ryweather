7f6ad687483a0e7eea1290b114f6be6fe8abf317
ashgan-dev <ashgan-dev@users.noreply.github.com>
2021-05-10 18:28:25 +0200

    correction des heures affichées
    les heures affichées ne prenaient pas en compte les heures des changements

--- ryweather.py
+++ ryweather.py
@@ -8,6 +8,7 @@ from decimal import Decimal
 import arrow
 import requests
 from dateutil.rrule import rrule, MINUTELY
+from dateutil.parser import *
 from flask import Flask, render_template
 
 
@@ -18,9 +19,6 @@ def get_cc():
     data = dict()
     data['cycles'] = list()
 
-    for dt in rrule(MINUTELY, dtstart=datetime.utcnow(), interval=3, count=120):
-        data['cycles'].append(arrow.get(dt).to('Europe/Paris').format('HH:mm:ss'))
-
     cycle_courant = str(decoded['cycle'])
     ingame_hour = Decimal(decoded['hour'])
     ingame_min = ingame_hour - int(ingame_hour)
@@ -29,15 +27,19 @@ def get_cc():
     real_delta_time = timedelta(seconds=min_left * 0.05 * 60)
     next_cc = arrow.utcnow() + real_delta_time
 
+    data['cycles'].append(arrow.utcnow().to('Europe/Paris').format('HH:mm:ss'))
+
+    for dt in rrule(MINUTELY, dtstart=parse(next_cc.format('YYYY-MM-DD HH:mm:ss ZZ')), interval=3, count=120):
+        data['cycles'].append(arrow.get(dt).to('Europe/Paris').format('HH:mm:ss'))
+
     if int(ingame_hour) % 3 == 0:
         for i in decoded['continents'].keys():
             data[i] = list()
             popped_cycle = decoded['continents'][i].pop(cycle_courant)
-
             data[i].append(float(Decimal(popped_cycle['value']) * 100))
             data[i].append(float(Decimal(popped_cycle['value']) * 100))
             data[i].append(float(Decimal(popped_cycle['value']) * 100))
-
+            # passer 3 valeurs
             for j in decoded['continents'][i].values():
                 data[i].append(float(Decimal(j['value']) * 100))
                 data[i].append(float(Decimal(j['value']) * 100))
@@ -46,27 +48,23 @@ def get_cc():
         for i in decoded['continents'].keys():
             data[i] = list()
             popped_cycle = decoded['continents'][i].pop(cycle_courant)
-
             data[i].append(float(Decimal(popped_cycle['value']) * 100))
             data[i].append(float(Decimal(popped_cycle['value']) * 100))
-
+            # passer 2 valeurs
             for j in decoded['continents'][i].values():
                 data[i].append(float(Decimal(j['value']) * 100))
                 data[i].append(float(Decimal(j['value']) * 100))
                 data[i].append(float(Decimal(j['value']) * 100))
     else:
-        print('etat: transition')
         for i in decoded['continents'].keys():
             data[i] = list()
             popped_cycle = decoded['continents'][i].pop(cycle_courant)
-
             data[i].append(float(Decimal(popped_cycle['value']) * 100))
-
+            # passer 1 valeurs
             for j in decoded['continents'][i].values():
                 data[i].append(float(Decimal(j['value']) * 100))
                 data[i].append(float(Decimal(j['value']) * 100))
                 data[i].append(float(Decimal(j['value']) * 100))
-
     return data
 
 
